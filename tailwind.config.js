87aa637fd224c88297c15a3b514383298988f572
mikamiAkito <akito.mikami17@gmail.com>
2024-10-18 00:15:43 +0900

    フロント環境構築

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,50 @@ export default {
     "./app/javascript/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+          sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+      scale: {
+          '450': '4.5',
+      },
+      keyframes: {
+          gnaviAnime: {
+              '0%': {
+                  opacity: '0'
+              },
+              '100%': {
+                  opacity: '1'
+              }
+          }
+      },
+      animation: {
+          gnaviAnime: 'gnaviAnime',
+      },
+      colors: {
+          'raba': 'rgba(0,0,0,0.4)'
+      },
+      height: {
+          '13': '3.2rem',
+          '18': '4.5rem'
+      },
+      zIndex: {
+          '25': '25',
+          '60': '60',
+      },
+      screens: {
+          xs: '240px',
+          sm: '480px',
+          md: '768px',
+          lg: '976px',
+          xl: '1440px',
+      },
+    },
   },
-  plugins: [],
+
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
+    require('tailwindcss-animated')
+  ],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss' //import * asの形に修正するとpostcssが認証されなくなる
+import autoprefixer from 'autoprefixer' //import * asの形に修正するとpostcssが認証されなくなる
 
 export default defineConfig({
   plugins: [
